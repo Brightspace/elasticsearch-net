adafcb6120cdc03576f992f0a0fd88ac8fa0cc25
Jeff Ashton <JeffAshton@users.noreply.github.com>
2020-06-24 14:05:40 -0400

    Setting up d2l builds of D2L.Nest.JsonNetSerializer (#1)
    * Setting up d2l builds of D2L.Nest.JsonNetSerializer

--- src/Nest.JsonNetSerializer/JsonReaderExtensions.cs
+++ src/Nest.JsonNetSerializer/JsonReaderExtensions.cs
@@ -20,15 +20,12 @@ namespace Nest.JsonNetSerializer
 			return token;
 		}
 
-		public static async Task<JToken> ReadTokenWithDateParseHandlingNoneAsync(this JsonReader reader,
+		public static Task<JToken> ReadTokenWithDateParseHandlingNoneAsync(this JsonReader reader,
 			CancellationToken cancellationToken = default(CancellationToken)
 		)
 		{
-			var dateParseHandling = reader.DateParseHandling;
-			reader.DateParseHandling = DateParseHandling.None;
-			var token = await JToken.ReadFromAsync(reader, cancellationToken).ConfigureAwait(false);
-			reader.DateParseHandling = dateParseHandling;
-			return token;
+			var token = ReadTokenWithDateParseHandlingNone( reader );
+			return Task.FromResult( token );
 		}
 	}
 }
